6618b9f4a625009c423eb96a2922564b920bb3c1
Julius Morselt <juliusmorselt@gmail.com>
2023-06-30 14:11:22 +0100

    google site verification

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
 import Header from '../components/Header/header'
+import Head from 'next/head'
 
 import { NextSeo } from 'next-seo'
 
@@ -11,6 +12,9 @@ export default function App({ Component, pageProps }: AppProps) {
         title='Julius Morselt'
         description='Hoi! Ik ben Julius, een frontend ontwikkelaar gevestigd Arnhem. Mijn passie ligt bij het bouwen van elegante en gebruiksvriendelijke websites. Ik streef ernaar om webinterfaces te creëren die zowel visueel aantrekkelijk als functioneel zijn, met oog voor een optimale gebruikerservaring. Ook ben ik bezig mijn kennis uit te breiden naar backend technologieën.'
       />
+      <Head>
+      <meta name="google-site-verification" content="N2fMtNV8qf38LyzbrKv2njRfCtYEOhwsNBEXzLIlkDg" />
+      </Head>
       <Header />
       <Component {...pageProps} />    
     </>
