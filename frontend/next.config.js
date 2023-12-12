0ab58b6bcc7351f694cec3322a81acc847540786
unknown <juliusmorselt@gmail.com>
2023-12-12 18:06:47 +0100

    env variable not getting used

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,10 +1,15 @@
 /** @type {import('next').NextConfig} */
+
+require("dotenv").config()
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  images: {
-		domains: ['cdn.sanity.io']
-	}
+    reactStrictMode: true,
+    swcMinify: true,
+    images: {
+        domains: ['cdn.sanity.io']
+    },
+    env: {
+        NEXT_PUBLIC_SANITY_PROJECT_ID: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
+    }
 }
 
 module.exports = nextConfig
