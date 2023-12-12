72abcf76598f2fb4ab36c0c90b1b7bc2f98990c6
unknown <juliusmorselt@gmail.com>
2023-12-12 18:21:15 +0100

    test push for config file

--- frontend/sanity.config.ts
+++ frontend/sanity.config.ts
@@ -2,8 +2,8 @@ import { deskTool } from 'sanity/desk';
 import { defineConfig } from 'sanity'
 import schemas from './sanity/schemas'
 
-export const config = defineConfig({
-    projectId: `${process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}`,
+export const config = ({
+    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
     dataset: 'production',
     title: 'Portfolio Website',
     apiVersion: '2023-07-07',
