59554acdb0cb7a7170c2e1ea02b2e668393adb55
unknown <juliusmorselt@gmail.com>
2023-12-12 18:24:37 +0100

    config file please work

--- frontend/sanity.config.ts
+++ frontend/sanity.config.ts
@@ -2,12 +2,12 @@ import { deskTool } from 'sanity/desk';
 import { defineConfig } from 'sanity'
 import schemas from './sanity/schemas'
 
-export const config = ({
-    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+export const config = {
+    projectId: `${process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}`,
     dataset: 'production',
     title: 'Portfolio Website',
     apiVersion: '2023-07-07',
     basePath: '/admin',
     plugins: [deskTool()],
     schema: { types: schemas }
-})
+}
